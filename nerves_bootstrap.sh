e599e626411de05a758a62278e7dea02aebe2740
vaibhavgupta3110 <vaibhav.gupta@me.com>
2017-03-06 00:48:41 +0530

    Update nerves_bootstrap.sh

--- nerves_bootstrap.sh
+++ nerves_bootstrap.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get -y upgrade
-apt-get -y -f install git g++ libssl-dev libncurses5-dev bc m4 make unzip libmnl-dev libssh-dev bison cmake automake autoconf build-essential libpq-dev
-apt-get -y -f install curl wget libtool python python-pip cpio bzip2 gcc python3-ply ncurses-dev python-yaml python2
+# housekeeping
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y -f install git g++ libssl-dev libncurses5-dev bc m4 make unzip libmnl-dev libssh-dev bison cmake automake autoconf build-essential libpq-dev
+sudo apt-get -y -f install curl wget libtool python python-pip cpio bzip2 gcc python3-ply ncurses-dev python-yaml python2
+sudo apt-get -y -f install openssl fop xsltproc unixodbc-dev 
+sudo apt-get -y -f install default-jdk
 
-# packages not recommended by nerves-sdk that seem to be required
-apt-get install -y zip unzip make
+#update apt sources
+echo "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main" | sudo tee -a /etc/apt/sources.list
+echo "deb https://packages.erlang-solutions.com/ubuntu xenial contrib" | sudo tee -a /etc/apt/sources.list
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 
-su vagrant <<'EOF'
-  mkdir -p /home/vagrant/.nerves-cache
-  git clone https://github.com/nerves-project/nerves-sdk.git /home/vagrant/nerves-sdk
-  cd /home/vagrant/nerves-sdk
-  make nerves_rpi_defconfig
-  make
-EOF
+#install rvm
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://get.rvm.io | bash -s stable
 
+#install asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.2.1
+echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
+echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
+
+#housekeeping
+sudo apt-get update
+sudo apt-get -y update
+sudo apt-get build-dep postgresql-common node nodejs 
