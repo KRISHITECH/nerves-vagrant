d53204afdf53c8b0bdc1334c315dc87964919b7a
vaibhavgupta3110 <vaibhav.gupta@me.com>
2017-03-06 14:45:33 +0530

    Update nerves_bootstrap.sh

--- nerves_bootstrap.sh
+++ nerves_bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# housekeeping
+# housekeeping & requirements
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y -f install git g++ libssl-dev libncurses5-dev bc m4 make unzip libmnl-dev libssh-dev bison cmake automake autoconf build-essential libpq-dev
@@ -17,6 +17,9 @@ curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 
+#install ruby and set as default
+rvm install ruby-2.4.0 --default --binary
+
 #install asdf
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.2.1
 echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
@@ -26,3 +29,8 @@ echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
 sudo apt-get update
 sudo apt-get -y update
 sudo apt-get build-dep postgresql-common node nodejs 
+
+#add asdf node, erlang & elixir
+asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
+asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
